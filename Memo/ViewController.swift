ca9167609c372999f804e47b81a6dea591eb0efb
Yongwan <wanrage@naver.com>
2019-05-31 11:33:36 +0900

    커스텀클래스 등록 다시 commit

--- Memo/ViewController.swift
+++ Memo/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         count+=1;
-        
+        count-=1;
     }
 
 
